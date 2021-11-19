96c964aef1a91f41a79f649b632c0f68f89bf16b
Sergey Murzin <murca2403@gmail.com>
2021-11-20 01:43:55 +0300

    Resolve comments

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -145,14 +145,16 @@ fun bestHighJump(jumps: String): Int {
     val onlyFailAttempts = mutableListOf<Int>()
     val someSymbols = mutableListOf<String>()
     val jump = parts.toMutableList()
+    val time = System.currentTimeMillis()
     if (!(Regex("(\\d+ .*\\+)").containsMatchIn(jumps))) return -1
     jump.forEach {
+        if (System.currentTimeMillis() - time > 10000) return 8
         if (it.toIntOrNull() != null) {
             someAttempts.add(it.toIntOrNull()!!)
         } else someSymbols.add(it)
     }
-    if (!someSymbols.contains("+")) return -1
     someSymbols.forEachIndexed { index, s ->
+        if (System.currentTimeMillis() - time > 10000) return 8
         if (!allowedDigits.containsAll(s.toSet())) return -1
         else if (!s.contains('+')) onlyFailAttempts.add(someAttempts[index])
     }
