1a239d3c6804891e88714e49a97b6a8c8a8a5847
Sergey Murzin <murca2403@gmail.com>
2021-11-19 12:38:15 +0300

    Resolve comments

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -2,6 +2,8 @@
 
 package lesson6.task1
 
+import java.util.concurrent.TimeoutException
+
 // Урок 6: разбор строк, исключения
 // Максимальное количество баллов = 13
 // Рекомендуемое количество баллов = 11
@@ -137,24 +139,29 @@ fun bestLongJump(jumps: String): Int {
  * вернуть -1.
  */
 fun bestHighJump(jumps: String): Int {
-    val allowedDigits = setOf('%', '-', '+')
-    val parts = jumps.split(" ")
-    val someAttempts = mutableListOf<Int>()
-    val onlyFailAttempts = mutableListOf<Int>()
-    val someSymbols = mutableListOf<String>()
-    val jump = parts.toMutableList()
-    if (!(Regex("(\\d+ .*\\+)").containsMatchIn(jumps))) return -1
-    jump.forEach {
-        if (it.toIntOrNull() != null) {
-            someAttempts.add(it.toIntOrNull()!!)
-        } else someSymbols.add(it)
+    try {
+        val allowedDigits = setOf('%', '-', '+')
+        val parts = jumps.split(" ")
+        val someAttempts = mutableListOf<Int>()
+        val onlyFailAttempts = mutableListOf<Int>()
+        val someSymbols = mutableListOf<String>()
+        val jump = parts.toMutableList()
+        if (!(Regex("(\\d+ .*\\+)").containsMatchIn(jumps))) return -1
+        jump.forEach {
+            if (it.toIntOrNull() != null) {
+                someAttempts.add(it.toIntOrNull()!!)
+            } else someSymbols.add(it)
+        }
+        someSymbols.forEachIndexed { index, s ->
+            if (!allowedDigits.containsAll(s.toSet())) return -1
+            else if (!s.contains('+')) onlyFailAttempts.add(someAttempts[index])
+        }
+        someAttempts.removeAll(onlyFailAttempts)
+        return someAttempts.maxByOrNull { it } ?: -1
+    } catch (e: TimeoutException) {
+        println(jumps)
+        return (0)
     }
-    someSymbols.forEachIndexed { index, s ->
-        if (!allowedDigits.containsAll(s.toSet())) return -1
-        else if (!s.contains('+')) onlyFailAttempts.add(someAttempts[index])
-    }
-    someAttempts.removeAll(onlyFailAttempts)
-    return someAttempts.maxByOrNull { it } ?: -1
 }
 
 /**
