b23484d8c5b73062926a6f5ed7ec264854eef1da
Sergey Murzin <murca2403@gmail.com>
2021-11-26 23:22:30 +0300

    Resolve comments

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -310,7 +310,43 @@ Suspendisse ~~et elit in enim tempus iaculis~~.
  * (Отступы и переносы строк в примере добавлены для наглядности, при решении задачи их реализовывать не обязательно)
  */
 fun markdownToHtmlSimple(inputName: String, outputName: String) {
-    TODO()
+    val result = StringBuilder().append("<html><body><p>")
+    val lines = File(inputName).bufferedReader().readLines()
+    lines.forEach { s ->
+        var str = s
+        var i = 0
+        if (str.isEmpty()) {
+            str = "</p><p>"
+        }
+        Regex("\\*\\*(.*?)\\*\\*").findAll(s, 0).forEach { _ -> i++ }
+        if (i != 0) {
+            for (j in 1..(i * 2)) {
+                str = if (j % 2 != 0) str.replaceFirst("**", ("<b>"))
+                else str.replaceFirst("**", ("</b>"))
+            }
+        }
+        var k = 0
+        str.forEach { if (it == '*') k++ }
+        if (k % 2 != 0) str = str.replace("***", "<b><i>")
+        Regex("\\*(.*?)\\*").findAll(s, 0).forEach { _ -> i++ }
+        if (i != 0) {
+            for (j in 1..(i * 2)) {
+                str = if (j % 2 != 0) str.replaceFirst("*", ("<i>"))
+                else str.replaceFirst("*", ("</i>"))
+            }
+        }
+        Regex("~~(.*?)~~").findAll(s, 0).forEach { _ -> i++ }
+        if (i != 0) {
+            for (j in 1..(i * 2)) {
+                str = if (j % 2 != 0) str.replaceFirst("~~", ("<s>"))
+                else str.replaceFirst("~~", ("</s>"))
+            }
+        }
+        result.append(str)
+    }
+    File(outputName).bufferedWriter().use {
+        it.write(result.append("</p></body></html>").toString())
+    }
 }
 
 /**
